9eb46c83fbc8cd5983dbbe9104708ca98be38c58
Yoav Nathaniel <yoav.nathaniel@armis.com>
2026-01-06 11:28:18 -0500

    chore: update armis-cli to v1.0.1

--- Formula/armis-cli.rb
+++ Formula/armis-cli.rb
@@ -1,30 +1,30 @@
 class ArmisCli < Formula
   desc "Enterprise-grade CLI tool for static application security scanning"
   homepage "https://github.com/ArmisSecurity/armis-cli"
-  version "1.0.0"
+  version "1.0.1"
   license "Apache-2.0"
 
   on_macos do
     on_intel do
-      url "https://github.com/ArmisSecurity/armis-cli/releases/download/v1.0.0/armis-cli-darwin-amd64.tar.gz"
-      sha256 "d655712a49e94fd0ae64e1186219d43cccab74c4f7cf55cbf5099dc12e039316"
+      url "https://github.com/ArmisSecurity/armis-cli/releases/download/v1.0.1/armis-cli-darwin-amd64.tar.gz"
+      sha256 "597226bbf8dad80baa09ca82b9302e8692250c115cfe5d042163865ad1a79326"
     end
 
     on_arm do
-      url "https://github.com/ArmisSecurity/armis-cli/releases/download/v1.0.0/armis-cli-darwin-arm64.tar.gz"
-      sha256 "e82789703bc778b057d298e7d149a341d6048c906a2415126c1d00ce10a32762"
+      url "https://github.com/ArmisSecurity/armis-cli/releases/download/v1.0.1/armis-cli-darwin-arm64.tar.gz"
+      sha256 "4cfd36a2eeec2b0de22f6de46ea18c2ecf66685c616e0b8a18fea2c8ea7abae0"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/ArmisSecurity/armis-cli/releases/download/v1.0.0/armis-cli-linux-amd64.tar.gz"
-      sha256 "ef2c7326e48514610b58069904f41b30340f7e92b875d657513307f1925ff8c2"
+      url "https://github.com/ArmisSecurity/armis-cli/releases/download/v1.0.1/armis-cli-linux-amd64.tar.gz"
+      sha256 "3d11e4109ad741bcbbee8b96ee5118aa8d046d2423762c5861601073a7642e27"
     end
 
     on_arm do
-      url "https://github.com/ArmisSecurity/armis-cli/releases/download/v1.0.0/armis-cli-linux-arm64.tar.gz"
-      sha256 "3b3d20635c201525629f7dcae094fb8f45c5c6b0409dcd960915b555da3f6133"
+      url "https://github.com/ArmisSecurity/armis-cli/releases/download/v1.0.1/armis-cli-linux-arm64.tar.gz"
+      sha256 "5346b08693a5f9ed6ef668842ba7ea7ff5ec7b2e3cd79a58d821edf54593020f"
     end
   end
 
@@ -33,6 +33,6 @@ class ArmisCli < Formula
   end
 
   test do
-    system "#{bin}/armis-cli", "version"
+    system bin/"armis-cli", "version"
   end
 end
