380a108eb640b4f89fc5d8eb05d00a164393d086
Yiftach Cohen <91976623+yiftach-armis@users.noreply.github.com>
2026-03-15 15:37:06 +0200

    chore: update armis-cli to v1.4.0

--- Formula/armis-cli.rb
+++ Formula/armis-cli.rb
@@ -1,30 +1,30 @@
 class ArmisCli < Formula
   desc "Enterprise-grade CLI tool for static application security scanning"
   homepage "https://github.com/ArmisSecurity/armis-cli"
-  version "1.3.0"
+  version "1.4.0"
   license "Apache-2.0"
 
   on_macos do
     on_intel do
-      url "https://github.com/ArmisSecurity/armis-cli/releases/download/v1.3.0/armis-cli-darwin-amd64.tar.gz"
-      sha256 "da67cae78288da78c2ff4e86b7601249862ccf22d5a069895211e7827d27d0f4"
+      url "https://github.com/ArmisSecurity/armis-cli/releases/download/v1.4.0/armis-cli-darwin-amd64.tar.gz"
+      sha256 "94205e352c6ae9becb9b9e9dd58de7b4de46a76ffe94fa31649277814625bef1"
     end
 
     on_arm do
-      url "https://github.com/ArmisSecurity/armis-cli/releases/download/v1.3.0/armis-cli-darwin-arm64.tar.gz"
-      sha256 "30da969773071409a13cce377145b75cc10822406f2f1cdfcf6e2341cc3ac97e"
+      url "https://github.com/ArmisSecurity/armis-cli/releases/download/v1.4.0/armis-cli-darwin-arm64.tar.gz"
+      sha256 "c75b23d6c19572b84c6fada28f10e158f68c416e70e0829a660bf80bb2857ef8"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/ArmisSecurity/armis-cli/releases/download/v1.3.0/armis-cli-linux-amd64.tar.gz"
-      sha256 "cd706380d343b5ae7a2735e7ff62139b815732f6d0c39c156836e6107e1c00dc"
+      url "https://github.com/ArmisSecurity/armis-cli/releases/download/v1.4.0/armis-cli-linux-amd64.tar.gz"
+      sha256 "4dbb17737fd565016d9a1c60933ccbc803f1329329f3ca6b706f945bc63a9d4e"
     end
 
     on_arm do
-      url "https://github.com/ArmisSecurity/armis-cli/releases/download/v1.3.0/armis-cli-linux-arm64.tar.gz"
-      sha256 "e0968c6e0cd69a386354451825a9ed900bd5adc598dd6a64c32b6dfe35e22bce"
+      url "https://github.com/ArmisSecurity/armis-cli/releases/download/v1.4.0/armis-cli-linux-arm64.tar.gz"
+      sha256 "9bc5a35cc530b2e26f7b2ad480a2f43e892aaac7d14504aca1c45f6251b83a41"
     end
   end
 
